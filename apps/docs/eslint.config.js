e665a5c518af83b0352ac31caa58a9f1534e103a
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2025-02-06 09:24:13 +0700

    feat: init relative path name

--- apps/docs/eslint.config.js
+++ apps/docs/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from "@sommhai/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
