78c3658c9214cebfefe4e46c157e5ec0e4a341c3
Suntoh <weerawit.j@gmail.com>
2025-03-18 18:23:07 +0700

    feat: routing architecture init

---
+++ apps/web/src/app/home/page.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+
+import EventCard from '../component/EventCard';
+
+function HomePage(): React.ReactElement {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h1>HomePage</h1>
+      <Link href='/event/create'>
+        <h1 className='font-bold text-blue-500'>Create an event</h1>
+      </Link>
+      <div>
+        <EventCard />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
