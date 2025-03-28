cacf9bbbaea72eab9dcd844a7496ebe4fcfadc74
Suntoh <weerawit.j@gmail.com>
2025-03-28 23:45:48 +0700

    feat: use variable colors

--- apps/liff/src/components/common/CreateEvent.tsx
+++ apps/liff/src/components/common/CreateEvent.tsx
@@ -5,7 +5,7 @@ function CreateEvent() {
   return (
     <div className='flex h-screen flex-col items-center justify-center'>
       <div className='inline-flex'>
-        <Button className='relative h-[56px] w-[345px] items-center justify-between rounded-3xl bg-[#F6BB0A] p-4 text-2xl font-semibold hover:bg-[#F8CF52]'>
+        <Button className='bg-orange-3 relative h-[56px] w-[345px] items-center justify-between rounded-3xl p-4 text-2xl font-semibold hover:bg-[#F8CF52]'>
           <div>Create Event</div>
           <div>+</div>
         </Button>
