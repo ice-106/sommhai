760fdc1241fb8df2cf62045a59169d4087e4c707
Suntoh <weerawit.j@gmail.com>
2025-03-29 00:14:10 +0700

    feat: chadCnUi debug

--- apps/liff/src/components/CreateEvent.tsx
+++ apps/liff/src/components/CreateEvent.tsx
@@ -1,11 +1,10 @@
 import { Button } from '@sommhai/ui/components/ui/button';
-import React from 'react';
 
 function CreateEvent() {
   return (
     <div className='flex h-screen flex-col items-center justify-center'>
       <div className='inline-flex'>
-        <Button className='bg-orange-3 relative h-[56px] w-[345px] items-center justify-between rounded-3xl p-4 text-2xl font-semibold hover:bg-[#F8CF52]'>
+        <Button className='bg-orange-3 hover:bg-orange-3-hover relative h-[56px] w-[345px] items-center justify-between rounded-3xl p-4 text-2xl font-semibold'>
           <div>Create Event</div>
           <div>+</div>
         </Button>
