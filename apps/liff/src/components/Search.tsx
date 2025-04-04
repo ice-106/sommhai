9cebdeb8990b117d4a28cd84b4e097d038a3b46d
Suntoh <weerawit.j@gmail.com>
2025-04-04 16:16:50 +0700

    fix: debug next build fail

--- apps/liff/src/components/Search.tsx
+++ apps/liff/src/components/Search.tsx
@@ -1,23 +1,40 @@
 'use client';
 import { Input } from '@sommhai/ui/components/ui/input';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+function Search({ placeholder }: { placeholder: string }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchContent />
+    </Suspense>
+  );
+}
+
+function SearchContent() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(term);
+    const params = new URLSearchParams(searchParams.toString());
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   }, 300);
+
   return (
     <div className='flex flex-1'>
       <Input
         defaultValue={searchParams.get('query')?.toString()}
-        placeholder={placeholder}
+        placeholder='Search...'
         onChange={(e) => handleSearch(e.target.value)}
       />
     </div>
   );
 }
+export default Search;
