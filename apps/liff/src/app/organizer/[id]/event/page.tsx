3562b72874f26560a85fc76d59f0a238d58fcb1d
ArcaneSand <time-2546@hotmail.com>
2025-04-05 01:27:04 +0700

    feat: event-info-pending

--- apps/liff/src/app/organizer/[id]/event/page.tsx
+++ apps/liff/src/app/organizer/[id]/event/page.tsx
@@ -1,9 +1,17 @@
+import HeaderBurgur from '@/components/HeaderBurgur';
+import { DeleteEventButton } from '@/components/ui/organizer/button';
 import { IconButtonGroup } from '@/components/ui/organizer/event/iconbutton';
 
 export default function page() {
   return (
-    <div className='flex h-screen w-screen items-center justify-center'>
-      <IconButtonGroup />
+    <div className='flex h-screen w-screen flex-col'>
+      <HeaderBurgur name='Event Title' />
+      <div className='flex flex-1 flex-col justify-between px-24 py-16'>
+        <IconButtonGroup />
+        <div className='mb-10 w-full'>
+          <DeleteEventButton />
+        </div>
+      </div>
     </div>
   );
 }
