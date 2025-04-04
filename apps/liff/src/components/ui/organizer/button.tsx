672c102ad7e53177fb8fc7efbb893f0f83f595a2
ArcaneSand <time-2546@hotmail.com>
2025-04-05 01:27:04 +0700

    feat: event-info-pending

--- apps/liff/src/components/ui/organizer/button.tsx
+++ apps/liff/src/components/ui/organizer/button.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@sommhai/ui/components/ui/button';
 import Link from 'next/link';
 
-export default function CreateEventButton() {
+export function CreateEventButton() {
   return (
     <div className='flex w-full flex-1'>
       <Link className='w-full' href='organizer/create'>
@@ -10,5 +10,13 @@ export default function CreateEventButton() {
         </Button>
       </Link>
     </div>
+  );
+}
+
+export function DeleteEventButton() {
+  return (
+    <Button className='h-[56px] w-full' variant='delete'>
+      Remove Event
+    </Button>
   );
 }
