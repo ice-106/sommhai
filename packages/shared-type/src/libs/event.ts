bf86316d716f9de55470dfcbd2f3877710de4e05
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2025-04-08 10:38:41 +0700

    refactor: organizer endpoint

--- packages/shared-type/src/libs/event.ts
+++ packages/shared-type/src/libs/event.ts
@@ -3,19 +3,21 @@ import { z } from 'zod';
 import { attendee } from './attendee';
 import { organizer } from './organizer';
 
-export const Event = z.object({
+export const eventBaseInfo = z.object({
   eid: z.string(),
   name: z.string(),
-  date: z.date().optional(),
-  time: z.date().optional(),
+  date: z.date(),
+  time: z.date(),
   picture: z.array(z.string()),
-  location: z.string().optional(),
-  description: z.string().optional(),
-  invite_list: z.number().optional(),
-  memory: z.string().optional(),
+  location: z.string(),
+  description: z.string().nullable(),
+  invite_list: z.number(),
+  memory: z.string().nullable(),
   host: z.string(),
   host_uid: z.string(),
-  attendees: z.array(attendee).optional(),
-  attending: z.array(attendee).optional(),
-  organizers: z.array(organizer).optional(),
+  attendees: z.array(attendee),
+  attendings: z.array(attendee),
+  organizers: z.array(organizer),
 });
+
+export const eventOrganizerInfo = eventBaseInfo;
