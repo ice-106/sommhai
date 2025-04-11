bde2d32b363de4ef138e1fec55cd5f68bac179d2
Suntoh <weerawit.j@gmail.com>
2025-04-11 13:25:18 +0700

    feat: settimer component

---
+++ apps/liff/src/app/test/timePicker/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import TimePickerSelect from '@/components/TimePicker';
+
+function PageTest() {
+  return (
+    <div className='flex w-screen flex-col'>
+      {/* <TimePicker /> */}
+      <TimePickerSelect />
+    </div>
+  );
+}
+
+export default PageTest;
