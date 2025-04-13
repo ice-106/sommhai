e87e4ede0faf714cc6ea69ad856d86e9ff32c05e
oatkup1a <voraphol12345@gmail.com>
2025-04-13 15:50:28 +0700

    fix: date in user.adapter and shared-types to dob

--- apps/api/src/modules/user/user.adapter.ts
+++ apps/api/src/modules/user/user.adapter.ts
@@ -7,7 +7,7 @@ export const UserAdapter = {
       user: user.pref_name,
       phone: user.phone,
       email: user.email,
-      dob: user.dob instanceof Date ? user.dob.toISOString() : user.dob,
+      dob: user.dob,
       pref_name: user.pref_name,
       first_name: user.first_name,
       last_name: user.last_name,
--- packages/shared-type/src/libs/user.ts
+++ packages/shared-type/src/libs/user.ts
@@ -4,7 +4,7 @@ export const userInfo = z.object({
   uid: z.string(),
   user: z.string(),
   email: z.string(),
-  dob: z.string().optional(),
+  dob: z.date().optional(),
   pref_name: z.string().optional(),
   first_name: z.string().optional(),
   last_name: z.string().optional(),
