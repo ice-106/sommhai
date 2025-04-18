1cff414335612c307d38065dbf0332c8ff5e2039
Suntoh <weerawit.j@gmail.com>
2025-04-18 17:16:35 +0700

    feat: fetch events, create event example

--- apps/liff/src/env.ts
+++ apps/liff/src/env.ts
@@ -2,5 +2,7 @@ export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://loca
 
 export const LIFF_ID = process.env.NEXT_PUBLIC_LIFF_ID || '';
 
+export const SERVER_URL = process.env.SERVER_URL || 'http://localhost:8080';
+
 export const CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '';
 export const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || '';
