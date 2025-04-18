d46a82a8707f00c5577f6b6fc6e7b8f8a48e4528
Suntoh <weerawit.j@gmail.com>
2025-04-18 17:16:35 +0700

    feat: fetch events, create event example

--- apps/liff/src/components/organizer/EventCard.tsx
+++ apps/liff/src/components/organizer/EventCard.tsx
@@ -1,39 +1,37 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import React from 'react';
 import { IoIosTimer } from 'react-icons/io';
 import { MdNavigateNext } from 'react-icons/md';
 
-export function EventCardTestStatic() {
-  const pathName = usePathname();
-  return (
-    <Link href={`${pathName}/event/123`}>
-      <div className='bg-white-bg flex w-[345px] max-w-[100%] flex-col justify-between gap-4 rounded-[25px] p-16 shadow-lg'>
-        <div className='flex justify-between'>
-          <h1 className='text-bold-20 font-inter truncate'>Badminton Competition.........................</h1>
-          <MdNavigateNext className='self-center' size={25} />
-        </div>
-        <div className='flex'>
-          <IoIosTimer className='mt-[4px]' color='orange' />
-          <p className='text-regular-16 text-orange-2'>Timer</p>
-        </div>
-        <p className='font-inter text-regular-16-low'>Event Details</p>
-      </div>
-    </Link>
-  );
-}
+// export function EventCardTestStatic() {
+//   const pathName = usePathname();
+//   return (
+//     <Link href={`${pathName}/event/123`}>
+//       <div className='bg-white-bg flex w-[345px] max-w-[100%] flex-col justify-between gap-4 rounded-[25px] p-16 shadow-lg'>
+//         <div className='flex justify-between'>
+//           <h1 className='text-bold-20 font-inter truncate'>Badminton Competition.........................</h1>
+//           <MdNavigateNext className='self-center' size={25} />
+//         </div>
+//         <div className='flex'>
+//           <IoIosTimer className='mt-[4px]' color='orange' />
+//           <p className='text-regular-16 text-orange-2'>Timer</p>
+//         </div>
+//         <p className='font-inter text-regular-16-low'>Event Details</p>
+//       </div>
+//     </Link>
+//   );
+// }
 
 interface EventCardProp {
   name: string;
   link: string;
 }
 
-export function EventCard({ name: name, link: link }: EventCardProp) {
-  const pathName = usePathname();
+function EventCard({ name: name, link: link }: EventCardProp) {
   return (
-    <Link href={`${pathName}/${link}/event`}>
+    <Link href={`organizer/${link}/event`}>
       <div className='bg-white-bg flex w-[345px] flex-col justify-between gap-4 rounded-[25px] p-16 shadow-lg'>
         <div className='flex justify-between'>
           <h1 className='text-bold-20 font-inter'>{name}</h1>
@@ -48,3 +46,5 @@ export function EventCard({ name: name, link: link }: EventCardProp) {
     </Link>
   );
 }
+
+export default EventCard;
