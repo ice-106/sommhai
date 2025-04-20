20735e97bf0702f58a27f21739432214b0d837ea
Time <time-2546@hotmail.com>
2025-04-20 09:49:18 +0700

    feat: dashboard mold

---
+++ apps/liff/src/components/organizer/dashboard/BarChart.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@sommhai/ui/components/ui/card';
+import { ChartConfig, ChartContainer } from '@sommhai/ui/components/ui/chart';
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from 'recharts';
+const chartData = [
+  { month: 'January', desktop: 186 },
+  { month: 'February', desktop: 305 },
+  { month: 'March', desktop: 237 },
+  { month: 'April', desktop: 73 },
+  { month: 'May', desktop: 209 },
+  { month: 'June', desktop: 214 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: 'hsl(var(--chart-1))',
+  },
+} satisfies ChartConfig;
+
+export function PreferChart() {
+  return (
+    <Card className='bg-white-pure rounded-24 mx-auto mt-[19px] min-h-[196px] max-w-full px-24 py-12'>
+      <CardHeader>
+        <CardTitle className='text-medium-16 mx-auto'>Bar Chart - Label</CardTitle>
+        <div className='mx-auto mt-8 h-1 w-[100%] border-[1px]'></div>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              top: 20,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              axisLine={false}
+              dataKey='month'
+              tickFormatter={(value) => value.slice(0, 3)}
+              tickLine={false}
+              tickMargin={10}
+            />
+            <Bar dataKey='desktop' fill='var(--color-desktop)' radius={8}>
+              <LabelList className='fill-foreground' fontSize={12} offset={12} position='top' />
+            </Bar>
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ apps/liff/src/components/organizer/dashboard/Collabsible.tsx
@@ -0,0 +1,104 @@
+'use client';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@sommhai/ui/components/ui/collapsible';
+import { useState } from 'react';
+import { CgProfile } from 'react-icons/cg';
+import { IoChevronDownOutline } from 'react-icons/io5';
+
+export function CollabsibleAccepted() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className='rounded-24 bg-white-pure mx-auto mt-[19px] w-[345px] pb-[6px] pl-[16px] pr-[9px] pt-[8px]'>
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <CollapsibleTrigger className='flex w-full items-center justify-between'>
+          <h1 className='text-semi-18'>Accept Invitation</h1>
+          <div className='flex w-[44px] items-center gap-8'>
+            <h1 className='text-semi-18 text-[#2ECC71]'>10</h1>
+            <IoChevronDownOutline
+              className={`mt-[3px] transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+            />
+          </div>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <div className='mt-12 flex gap-12'>
+            <CgProfile className='mt-[3px]' />
+            <h1 className='text-semi-18'>Attendee A</h1>
+          </div>
+          <div className='mt-12 flex gap-12'>
+            <CgProfile />
+            <h1 className='text-semi-18'>Attendee A</h1>
+          </div>
+          <div className='mt-12 flex gap-12'>
+            <CgProfile />
+            <h1 className='text-semi-18'>Attendee A</h1>
+          </div>
+        </CollapsibleContent>
+      </Collapsible>
+    </div>
+  );
+}
+
+export function CollabsibleDenied() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className='rounded-24 bg-white-pure mx-auto mt-[19px] w-[345px] pb-[6px] pl-[16px] pr-[9px] pt-[8px]'>
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <CollapsibleTrigger className='flex w-full items-center justify-between'>
+          <h1 className='text-semi-18'>Denied Invitation</h1>
+          <div className='flex w-[44px] items-center gap-8'>
+            <h1 className='text-semi-18 text-[#E74C3C]'>10</h1>
+            <IoChevronDownOutline
+              className={`mt-[3px] transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+            />
+          </div>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <div className='mt-12 flex gap-12'>
+            <CgProfile className='mt-[3px]' />
+            <h1 className='text-semi-18'>Attendee A</h1>
+          </div>
+          <div className='mt-12 flex gap-12'>
+            <CgProfile />
+            <h1 className='text-semi-18'>Attendee A</h1>
+          </div>
+          <div className='mt-12 flex gap-12'>
+            <CgProfile />
+            <h1 className='text-semi-18'>Attendee A</h1>
+          </div>
+        </CollapsibleContent>
+      </Collapsible>
+    </div>
+  );
+}
+
+export function CollabsibleUnanswered() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className='rounded-24 bg-white-pure mx-auto mt-[19px] w-[345px] pb-[6px] pl-[16px] pr-[9px] pt-[8px]'>
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <CollapsibleTrigger className='flex w-full items-center justify-between'>
+          <h1 className='text-semi-18'>Unanswered Invitation</h1>
+          <div className='flex w-[44px] items-center gap-8'>
+            <h1 className='text-semi-18 text-[#F6BB0A]'>10</h1>
+            <IoChevronDownOutline
+              className={`mt-[3px] transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+            />
+          </div>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <div className='mt-12 flex gap-12'>
+            <CgProfile className='mt-[3px]' />
+            <h1 className='text-semi-18'>Attendee A</h1>
+          </div>
+          <div className='mt-12 flex gap-12'>
+            <CgProfile />
+            <h1 className='text-semi-18'>Attendee A</h1>
+          </div>
+          <div className='mt-12 flex gap-12'>
+            <CgProfile />
+            <h1 className='text-semi-18'>Attendee A</h1>
+          </div>
+        </CollapsibleContent>
+      </Collapsible>
+    </div>
+  );
+}
