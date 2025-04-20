6a7b3d355284825130e1a1f26c5227cabe6ef2c0
Suntoh <weerawit.j@gmail.com>
2025-04-20 12:04:29 +0700

    fix: build fail

--- packages/ui/src/components/ui/calendar.tsx
+++ packages/ui/src/components/ui/calendar.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { buttonVariants } from '@ui/components/ui/button';
-import { cn } from '@ui/lib/utils';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import * as React from 'react';
 import { DayPicker } from 'react-day-picker';
+
+import { cn } from '../../lib/utils';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
