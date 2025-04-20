1e5e509f7ef52d29b33ce822e194d43beace5321
Time <time-2546@hotmail.com>
2025-04-20 09:49:18 +0700

    feat: dashboard mold

---
+++ apps/liff/src/components/organizer/dashboard/PeferenceCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PeferenceCard() {
+  return (
+    <div className='bg-white-pure rounded-24 mx-auto mt-[19px] min-h-[196px] max-w-full px-24 py-12'>
+      <h1 className='text-medium-16'>PeferenceCard</h1>
+    </div>
+  );
+}
+
+export default PeferenceCard;
