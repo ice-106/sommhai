f3d215a9210a401f1d72040d055023c7970db58f
Suntoh <weerawit.j@gmail.com>
2025-04-20 14:32:00 +0700

    fix: organizer page

--- apps/liff/src/app/organizer/event/[id]/google-calendar/page.tsx
+++
@@ -1,8 +0,0 @@
-function CalendarPage() {
-  return (
-    <div className='flex h-screen w-screen items-center justify-center bg-blue-400'>
-      {/* <IconButton icon={} title="Google Calendar" link="/calendar" /> */}
-    </div>
-  );
-}
-export default CalendarPage;
