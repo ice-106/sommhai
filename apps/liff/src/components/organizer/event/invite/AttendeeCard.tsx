fcd1ef839660512d4d7bf157602ede2b38f715a5
o0NoN0o <pichanon.roong@outlook.com>
2025-04-22 02:21:18 +0700

    feat: fetch attendee data

--- apps/liff/src/components/organizer/event/invite/AttendeeCard.tsx
+++ apps/liff/src/components/organizer/event/invite/AttendeeCard.tsx
@@ -10,7 +10,7 @@ function AttendeeCard({ name: name }: AttendeeCardProp) {
   return (
     <div className='bg-white-bg flex w-[345px] flex-col justify-between gap-4 rounded-[25px] p-16 shadow-lg'>
       <div className='flex justify-between'>
-        <h1 className='text-bold-20 font-inter'>{name}</h1>
+        <h1 className='text-bold-20 truncate'>{name}</h1>
       </div>
     </div>
   );
