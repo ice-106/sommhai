50cefb6d2099d5518f51c756258d651bdd812f8e
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2025-04-22 13:42:17 +0700

    fix: install dotenv

--- apps/liff/src/env.ts
+++ apps/liff/src/env.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
 
 export const LIFF_ID = process.env.NEXT_PUBLIC_LIFF_ID || '';
