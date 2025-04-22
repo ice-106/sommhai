041ed8e0e72560c3a3e3abe9f8732b0a5a9f08b8
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2025-04-23 00:59:21 +0700

    refactor: naming secret

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -4,5 +4,5 @@ export const PORT = process.env.PORT || 8080;
 
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
 
-export const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET || '';
-export const LINE_CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || '';
+export const LINE_MSG_API_CHANNEL_SECRET = process.env.LINE_MSG_API_CHANNEL_SECRET || '';
+export const LINE_MSG_API_CHANNEL_ACCESS_TOKEN = process.env.LINE_MSG_API_CHANNEL_ACCESS_TOKEN || '';
