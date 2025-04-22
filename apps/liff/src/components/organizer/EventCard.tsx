306697d3463913329f83e02e7b3ff748c33b1833
o0NoN0o <pichanon.roong@outlook.com>
2025-04-22 17:19:48 +0700

    feat: event timer

--- apps/liff/src/components/organizer/EventCard.tsx
+++ apps/liff/src/components/organizer/EventCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { differenceInCalendarDays } from 'date-fns';
 import Link from 'next/link';
 import React from 'react';
 import { IoIosTimer } from 'react-icons/io';
@@ -27,9 +28,12 @@ import { MdNavigateNext } from 'react-icons/md';
 interface EventCardProp {
   name: string;
   link: string;
+  date: Date;
 }
 
-function EventCard({ name: name, link: link }: EventCardProp) {
+function EventCard({ name: name, link: link, date: date }: EventCardProp) {
+  const dayscal = differenceInCalendarDays(new Date(date), new Date());
+  console.log(dayscal);
   return (
     <Link href={`organizer/event/${link}`}>
       <div className='bg-white-bg flex w-[345px] flex-col justify-between gap-4 rounded-[25px] p-16 shadow-lg'>
@@ -39,7 +43,7 @@ function EventCard({ name: name, link: link }: EventCardProp) {
         </div>
         <div className='flex'>
           <IoIosTimer className='text-orange-2 mt-[4px]' />
-          <p className='text-regular-16 text-orange-2'>Timer</p>
+          <p className='text-regular-16 text-orange-2'>{dayscal.toString()} Days Left</p>
         </div>
         <p className='font-inter text-regular-16-low'>Event Details</p>
       </div>
