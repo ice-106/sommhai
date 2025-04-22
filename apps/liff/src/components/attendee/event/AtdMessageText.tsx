41ab88c8daf5cc3e59c458a809ef757265a9b924
o0NoN0o <pichanon.roong@outlook.com>
2025-04-22 20:33:23 +0700

    feat: attendee event

---
+++ apps/liff/src/components/attendee/event/AtdMessageText.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AtdMessage() {
+  return (
+    <div className='bg-orange-3 text-bold-24 flex min-h-[188px] w-[302px] flex-col items-center justify-center gap-24 overflow-y-auto rounded-[25px] p-[16px]'>
+      <p>Welcome Everyone!</p>
+    </div>
+  );
+}
+
+export default AtdMessage;
