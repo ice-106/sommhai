b82ddc3a0a517af8fc769027b9b9901d2a48a65e
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2025-04-23 00:54:12 +0700

    feat: line webhook

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -3,3 +3,6 @@ import 'dotenv/config';
 export const PORT = process.env.PORT || 8080;
 
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
+
+export const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET || '';
+export const LINE_CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || '';
