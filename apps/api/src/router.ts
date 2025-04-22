5a7d4b30a812210f3a1c6dc6adbf08466ba7e9e0
oatkup1a <voraphol12345@gmail.com>
2025-04-22 13:59:10 +0700

    fix: delete unsued example post

--- apps/api/src/router.ts
+++ apps/api/src/router.ts
@@ -4,13 +4,11 @@ import { RouterImplementation } from '@ts-rest/express/src/lib/types';
 
 import { AttendeeController } from './modules/attendee/attendee.controller';
 import { OrganizerController } from './modules/organizer/organizer.controller';
-import { PostController } from './modules/post/post.controller';
 import { UserController } from './modules/user/user.controller';
 
 const s = initServer();
 
 export const router: RouterImplementation<typeof contract> = s.router(contract, {
-  post: PostController,
   user: UserController,
   organizer: OrganizerController,
   attendee: AttendeeController,
