03dc9cb54ac20bbad18414cc749669a6cca8ca4f
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2025-04-22 15:49:44 +0700

    fix: next env

--- apps/liff/src/contexts/global/liff.tsx
+++ apps/liff/src/contexts/global/liff.tsx
@@ -31,7 +31,6 @@ export function LiffProvider({ children }: { children: React.ReactNode }): React
             setLiffObject(liff);
           })
           .catch((error: Error) => {
-            console.log('LIFF ID', LIFF_ID);
             console.error('LIFF init failed.');
             setLiffError(error.toString());
           })
