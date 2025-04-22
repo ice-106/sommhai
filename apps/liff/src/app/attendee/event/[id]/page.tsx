78ae6741858587b01a33901462a3467ec1636582
Suntoh <weerawit.j@gmail.com>
2025-04-22 15:54:33 +0700

    feat: detail event

---
+++ apps/liff/src/app/attendee/event/[id]/page.tsx
@@ -0,0 +1,60 @@
+'use client';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import HeaderBurgur from '@/components/common/HeaderBurgur';
+import DetailForm from '@/components/organizer/event/detail/DetailForm';
+
+function DetailPage() {
+  const params = useParams();
+  const id = params?.id as string;
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    if (currentPage !== 0) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+
+    // Cleanup on unmount
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [currentPage]);
+
+  const handleChangePage = (page: number) => {
+    setCurrentPage(page);
+  };
+  return (
+    <div className='flex h-screen w-screen flex-col'>
+      <HeaderBurgur name={id} />
+      <div className='flex flex-1 flex-col items-center justify-between gap-[10vh] px-24 py-32'>
+        <div
+          className='bg-orange-6 flex h-full w-full flex-col justify-center rounded-3xl text-center active:bg-gray-200'
+          onClick={() => handleChangePage(2)}
+        >
+          <h1 className='text-semi-24'>Event Details</h1>
+          <p className='text-medium-20'>Click to edit</p>
+        </div>
+        <div
+          className='bg-orange-3 rounded-24 active:bg-orange-4 flex h-full w-full flex-col justify-center text-center'
+          onClick={() => handleChangePage(3)}
+        >
+          <h1 className='text-semi-24'>Message</h1>
+          <p className='text-medium-20'>Click to edit</p>
+        </div>
+      </div>
+      {currentPage !== 0 && (
+        <div
+          className={`fixed left-0 top-0 z-50 h-screen w-screen overflow-y-auto transition-all duration-500 ease-in-out ${
+            currentPage !== 0 ? 'translate-y-0' : 'translate-y-full'
+          }`}
+        >
+          <DetailForm eid={id} page={currentPage} onClose={() => setCurrentPage(0)} />
+        </div>
+      )}
+    </div>
+  );
+}
+export default DetailPage;
