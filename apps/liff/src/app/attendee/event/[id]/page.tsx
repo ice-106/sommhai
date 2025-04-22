5dabb41c0f2df526b719e64446661619e6122250
o0NoN0o <pichanon.roong@outlook.com>
2025-04-22 20:33:23 +0700

    feat: attendee event

--- apps/liff/src/app/attendee/event/[id]/page.tsx
+++ apps/liff/src/app/attendee/event/[id]/page.tsx
@@ -1,7 +1,9 @@
 'use client';
+import { Circle, User } from 'lucide-react';
 import { useParams } from 'next/navigation';
 
 import AtdEventText from '@/components/attendee/event/AtdEventText';
+import AtdMessage from '@/components/attendee/event/AtdMessageText';
 import HeaderBurgur from '@/components/common/HeaderBurgur';
 
 export default function AtdEventPage() {
@@ -14,8 +16,17 @@ export default function AtdEventPage() {
         <div className='bg-orange-3 flex h-[420px] w-[300px] flex-col justify-center text-center'>
           <h1 className='text-semi-24'>Photo</h1>
         </div>
-        <div>
+        <div className='flex flex-col items-center justify-center gap-24'>
           <AtdEventText />
+        </div>
+        <div className='flex flex-col items-center justify-center gap-24'>
+          <AtdMessage />
+          <div className='flex w-full items-center justify-center gap-16'>
+            <Circle className='text-grey-light fill-grey-light'>
+              <User className='text-black-pure !size-40' />
+            </Circle>
+            <h1 className='text-bold-20'>Organizer A</h1>
+          </div>
         </div>
       </div>
     </div>
