3618376a461ec5ac1d4a6a39c8ca1ad431de4988
Suntoh <weerawit.j@gmail.com>
2025-04-22 15:54:33 +0700

    feat: detail event

--- apps/liff/src/components/organizer/event/detail/EventTextForm.tsx
+++ apps/liff/src/components/organizer/event/detail/EventTextForm.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
-function EventTextForm() {
+function EventTextForm({
+  name,
+  address,
+  date,
+  description,
+}: {
+  name: string;
+  address: string;
+  date: string;
+  description: string;
+}) {
   return (
     <div
       className={`bg-white-pure flex min-h-[236px] w-[333px] flex-col items-center gap-24 overflow-y-auto rounded-[25px] p-[16px]`}
       onClick={(e) => e.stopPropagation()}
     >
-      <p>MyEvent</p>
-      <p>31 / 3 / 2025 , 20:00</p>
-      <p>1/1 rama 1 road ....</p>
-      <p>Some description. Please come</p>
+      <h1 className='text-3xl'>{name}</h1>
+      <p>{address}</p>
+      <p>{date}</p>
+      <p>{description}</p>
     </div>
   );
 }
