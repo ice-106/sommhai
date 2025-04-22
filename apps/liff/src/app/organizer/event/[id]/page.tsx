89552bac48dae85e452ba0b248830282e8907877
Suntoh <weerawit.j@gmail.com>
2025-04-23 00:43:20 +0700

    feat: integration history

--- apps/liff/src/app/organizer/event/[id]/page.tsx
+++ apps/liff/src/app/organizer/event/[id]/page.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'next/navigation';
 
 import HeaderBurgur from '@/components/common/HeaderBurgur';
 import { DeleteEventButton } from '@/components/organizer/buttons';
+import CompleteButton from '@/components/organizer/completeButton';
 import { IconButtonGroup } from '@/components/organizer/iconbutton';
 
 export default function EventPage() {
@@ -15,8 +16,9 @@ export default function EventPage() {
       <div className='mt-[10px] flex flex-1 flex-col justify-between px-24 py-16'>
         <IconButtonGroup />
 
-        <div className='my-16 w-full'>
+        <div className='my-16 flex w-full flex-col items-center justify-center gap-4'>
           <DeleteEventButton id={id} />
+          <CompleteButton eid={id} />
         </div>
       </div>
     </div>
---
+++ apps/liff/src/components/organizer/completeButton.tsx
@@ -0,0 +1,43 @@
+'use client';
+import { Button } from '@sommhai/ui/components/ui/button';
+import Link from 'next/link';
+import { useContext } from 'react';
+
+import { LiffContext } from '@/contexts/global/liff';
+import { API_BASE_URL } from '@/env';
+
+function CompleteButton({ eid }: { eid: string }) {
+  const { userId } = useContext(LiffContext); // Replace with actual user ID
+  const handleClick = () => {
+    fetch(`${API_BASE_URL}/users/history`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        eid: eid,
+        uid: userId,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        console.log('Event added to history successfully', res);
+      } else {
+        console.error('Failed to add event to history');
+      }
+    });
+  };
+  return (
+    <div className='flex w-full flex-col items-center justify-center pt-2'>
+      <Link className='inline-flex w-full' href={'/history'}>
+        <Button
+          className='bg-orange-3 hover:bg-orange-3-hover relative h-56 w-full items-center justify-between gap-40 rounded-3xl px-20 py-20 text-2xl font-semibold'
+          onClick={handleClick}
+        >
+          <div className='absolute left-0 top-0 h-56 w-full rounded-3xl shadow-[0px_4px_4px_0px_rgba(0,0,0,0.3)]'></div>
+          <div className='w-full justify-start text-center font-medium'>Completed</div>
+        </Button>
+      </Link>
+    </div>
+  );
+}
+export default CompleteButton;
