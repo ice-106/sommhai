bc31486a7d1711fbbb7973a733568c85d5ed0efd
Suntoh <weerawit.j@gmail.com>
2025-04-22 21:21:32 +0700

    feat: delete button integration

--- apps/liff/src/app/organizer/event/[id]/page.tsx
+++ apps/liff/src/app/organizer/event/[id]/page.tsx
@@ -16,9 +16,14 @@ export default function EventPage() {
       <div className='mt-[10px] flex flex-1 flex-col justify-between px-24 py-16'>
         <IconButtonGroup />
 
+<<<<<<< HEAD
         <div className='my-16 flex w-full flex-col items-center justify-center gap-4'>
           <DeleteEventButton id={id} />
           <CompleteButton eid={id} />
+=======
+        <div className='my-16 w-full'>
+          <DeleteEventButton id={id} />
+>>>>>>> 0247e9b (feat: delete button integration)
         </div>
       </div>
     </div>
