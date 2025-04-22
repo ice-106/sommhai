e64edf5fe11f3a2dfe0dc7bc9ce14d401e41fa70
Suntoh <weerawit.j@gmail.com>
2025-04-22 22:21:00 +0700

    feat: event detail integration

--- apps/api/src/modules/organizer/organizer.service.ts
+++ apps/api/src/modules/organizer/organizer.service.ts
@@ -169,6 +169,11 @@ export const OrganizerService = {
     try {
       const event = await prisma.event.findUnique({
         where: { eid: eventId },
+        include: {
+          attendees: true,
+          attendings: true,
+          organizers: true,
+        },
       });
 
       if (!event) {
