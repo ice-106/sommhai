4b99e1ca46d6d9d02d28a2ad3f4f825218e273fa
Suntoh <weerawit.j@gmail.com>
2025-04-22 21:21:32 +0700

    feat: delete button integration

--- apps/liff/src/app/organizer/event/[id]/page.tsx
+++ apps/liff/src/app/organizer/event/[id]/page.tsx
@@ -1,17 +1,22 @@
+'use client';
+import { useParams } from 'next/navigation';
+
 import HeaderBurgur from '@/components/common/HeaderBurgur';
 import { DeleteEventButton } from '@/components/organizer/buttons';
 import { IconButtonGroup } from '@/components/organizer/iconbutton';
 
 export default function EventPage() {
-  const name = 'Event Name'; // Replace with server data
+  const param = useParams();
+  // Extract event id from the params object
+  const id = param.id as string;
   return (
     <div className='flex h-screen w-screen flex-col'>
-      <HeaderBurgur name={name} />
+      <HeaderBurgur name={'My Event'} />
       <div className='mt-[10px] flex flex-1 flex-col justify-between px-24 py-16'>
         <IconButtonGroup />
 
         <div className='my-16 w-full'>
-          <DeleteEventButton />
+          <DeleteEventButton id={id} />
         </div>
       </div>
     </div>
