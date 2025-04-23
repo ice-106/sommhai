96d0eac7a28482063a0f81312205aaf46fc5890a
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2025-04-23 10:14:37 +0700

    feat: line profile

--- apps/liff/src/app/profile/page.tsx
+++ apps/liff/src/app/profile/page.tsx
@@ -1,10 +1,37 @@
 'use client';
+import liff from '@line/liff';
 import { User } from 'lucide-react';
-import React from 'react';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
 
 import HeaderBurgur from '@/components/common/HeaderBurgur';
+import { LIFF_ID } from '@/env';
+
+interface LineProfile {
+  displayName: string;
+  userId: string;
+  pictureUrl?: string;
+  statusMessage?: string;
+}
 
 function Profile() {
+  const [profile, setProfile] = useState<LineProfile | undefined>();
+  useEffect(() => {
+    async function fetchProfile() {
+      await liff
+        .init({ liffId: LIFF_ID })
+        .then(() => liff.getProfile())
+        .then((res) => res)
+        .then((data) => {
+          setProfile(data as LineProfile);
+        })
+        .catch((error) => {
+          console.error('Error fetching profile:', error);
+        });
+    }
+    fetchProfile();
+  }, [profile]);
+
   return (
     <div className='bg-orange-4 flex max-h-screen w-screen flex-col'>
       <HeaderBurgur name='Profile' />
@@ -14,7 +41,11 @@ function Profile() {
         {/* Curved white section at bottom of yellow area */}
         <div className='bg-white-bg absolute mt-[8vh] h-[80vh] w-[150vw] rounded-full'></div>
         <div className='bg-grey-light absolute mt-[2vh] flex h-[20vw] w-[20vw] items-center justify-center rounded-full'>
-          <User className='text-black-pure h-[20vw]' />
+          {profile?.pictureUrl ? (
+            <Image alt='Profile' height={290} src={profile.pictureUrl} width={290} />
+          ) : (
+            <User className='text-black-pure h-[20vw]' />
+          )}
         </div>
       </div>
 
@@ -24,7 +55,7 @@ function Profile() {
         <div className='relative mb-6 w-full max-w-xs'>
           <div className='bg-white-pure w-full rounded-3xl p-5 shadow-sm'>
             <p className='text-black-pure text-lg font-medium'>
-              Line Name: <span className='font-bold'>Organizer A</span>
+              Line Name: <span className='font-bold'>{profile?.displayName}</span>
             </p>
           </div>
         </div>
