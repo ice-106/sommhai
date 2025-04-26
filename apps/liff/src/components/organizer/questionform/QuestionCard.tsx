de6cc8ea74f0a8738c98ad9304c1d6b0593746e7
Suntoh <weerawit.j@gmail.com>
2025-04-26 14:59:09 +0700

    feat: org question page

--- apps/liff/src/components/organizer/questionform/QuestionCard.tsx
+++ apps/liff/src/components/organizer/questionform/QuestionCard.tsx
@@ -8,14 +8,16 @@ import { CheckedboxQuestions, MultipleQuestions } from './MultipleQuestions';
 type QuestionCardProps = {
   text: string;
   qid: string;
+  options?: string[];
   type: string;
   isRequired: boolean;
   onDelete?: (qid: string) => void;
 };
 
-function QuestionCard({ text, qid, type, isRequired, onDelete }: QuestionCardProps) {
+function QuestionCard({ text, qid, type, isRequired, onDelete, options }: QuestionCardProps) {
   const [question, setQuestion] = useState(text);
   const [questionType, setQuestionType] = useState(type);
+  console.log(options);
   return (
     <div className='bg-white-pure rounded-24 mx-auto mt-2 flex h-full w-full flex-col px-[24px] py-12'>
       <div className='mt-[7.5px]'>
@@ -38,17 +40,20 @@ function QuestionCard({ text, qid, type, isRequired, onDelete }: QuestionCardPro
         </Select>
       </div>
       <div className='mt-12 flex w-full flex-col'>
-        {questionType === 'SHORT_ANSWER' && ShortQuestion()}
-        {questionType === 'MULTIPLE_CHOICE' && <MultipleQuestions />}
-        {questionType === 'CHECKBOX' && <CheckedboxQuestions />}
+        {questionType === 'SHORT_ANSWER' && ShortQuestion(text)}
+        {questionType === 'MULTIPLE_CHOICE' && <MultipleQuestions options={options} question={text} />}
+        {questionType === 'CHECKBOX' && <CheckedboxQuestions options={options} question={text} />}
       </div>
     </div>
   );
 }
 
-function ShortQuestion() {
+function ShortQuestion(question: string) {
   return (
-    <Input className='focus-visible:border-orange-1 shadow-orange-5 !border-l-0 !border-r-0 !border-t-0 border-b-2 border-b-gray-300 shadow-md shadow-none focus-visible:outline-none focus-visible:ring-0' />
+    <Input
+      className='focus-visible:border-orange-1 shadow-orange-5 !border-l-0 !border-r-0 !border-t-0 border-b-2 border-b-gray-300 shadow-md focus-visible:outline-none focus-visible:ring-0'
+      value={question}
+    />
   );
 }
 
